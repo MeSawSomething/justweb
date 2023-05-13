6c1b993a57921a6a497c0293289fcaa5fcb0a35b
MeSawSomething <miso.hong.1989@gmail.com>
2023-05-13 17:45:44 +0900

    Feat: Service contents edit

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ServiceItem({ title, icon, description }) {
   return (
-    <a href="https://miso-astro-blog.vercel.app/">
+    <a href="https://miso-astro-blog.vercel.app/" target="_blank">
       <div className="bg-slate-300 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 hover:-translate-y-2 transform transition p-5 rounded-md">
         <div className="text-gray-800 dark:text-gray-300 w-6 h-6 mb-3">
           {icon}
--- src/data/services.jsx
+++ src/data/services.jsx
@@ -1,21 +1,24 @@
-import { MdWeb } from 'react-icons/md'
-import { FaRegHandshake } from 'react-icons/fa'
-import { VscCode } from 'react-icons/vsc'
+import { MdWeb } from "react-icons/md";
+import { FaRegHandshake } from "react-icons/fa";
+import { VscCode } from "react-icons/vsc";
 
 export default [
   {
-    title: '웹 개발',
-    description: '5개월간 2개의 팀프로젝트를 수행했습니다.',
-    icon: <MdWeb className='w-full h-full' />
+    title: "웹 개발",
+    description:
+      "5개월간 2개의 팀프로젝트 - 첫프로젝트는 클론코딩, 두번째로는 기업연계 멘토링 프로젝트- 를 수행했습니다.",
+    icon: <MdWeb className="w-full h-full" />,
   },
   {
-    title: '협업을 위한 커뮤니케이션',
-    description: '주어진 시간내에 완료해야하는 프로젝트. 어떤 환경에서도 해결과제를 위해 팀과 소통하는 법을 배웠습니다.',
-    icon: <FaRegHandshake className='w-full h-full' />
+    title: "협업을 위한 커뮤니케이션",
+    description:
+      "주어진 시간내에 완료해야하는 프로젝트. 어떤 환경에서도 해결과제를 위해 팀과 소통하는 법을 배웠습니다.",
+    icon: <FaRegHandshake className="w-full h-full" />,
   },
   {
-    title: '툴, 협업 방식에 대한 고민',
-    description: '협업을 위한 깃 컨벤션, 어떤 패키지 매니저를 고를까? 왜 Next.js를 써야할까? 고민하며 임했습니다.',
-    icon: <VscCode className='w-full h-full' />
+    title: "툴, 협업 방식에 대한 고민",
+    description:
+      "협업을 위한 깃 컨벤션, 어떤 패키지 매니저를 고를까? 왜 Next.js를 써야할까? 고민하는 법을 배웠습니다.",
+    icon: <VscCode className="w-full h-full" />,
   },
-]
+];
