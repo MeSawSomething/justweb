be1f453af1a05badaa964d3a3d70955833785915
MeSawSomething <miso.hong.1989@gmail.com>
2023-05-15 05:34:07 +0900

    Fix: image route edit.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,12 @@ function About() {
       <div className="w-full md:w-6/12">
         <SectionTitle>About Me</SectionTitle>
         <p className="text-sm text-gray-600 dark:text-gray-300">
-          한창 일할 나이에 개발공부 시작했다가 큰 코 다치는 중입니다.<br /> 어린친구들과 프로젝트를 하며 여러모로 많이 배우고 있습니다. 리액트의 ㄹ도 모르는 상태에서 Next.js와 타입스크립트로 프로젝트를 진행하며, 내가 지금 이걸 이해하는게 맞나? 싶지만 계속 공부하며 기본기를 다져나가고자 합니다. 하나 둘 사모으는 기술서적을 천천히 익혀나가고 있습니다. <br />
+          한창 일할 나이에 개발공부 시작했다가 큰 코 다치는 중입니다.
+          <br /> 어린친구들과 프로젝트를 하며 여러모로 많이 배우고 있습니다.
+          리액트의 ㄹ도 모르는 상태에서 Next.js와 타입스크립트로 프로젝트를
+          진행하며, 내가 지금 이걸 이해하는게 맞나? 싶지만 계속 공부하며
+          기본기를 다져나가고자 합니다. 하나 둘 사모으는 기술서적을 천천히
+          익혀나가고 있습니다. <br />
           맛집을 좋아합니다.
         </p>
         <a
@@ -18,7 +23,7 @@ function About() {
         </a>
       </div>
       <img
-        src="./assets/img/profile.jpeg"
+        src="/assets/img/profile.jpeg"
         alt="Miso"
         className="w-full md:w-5/12 rounded-lg object-fit py-1"
       />
